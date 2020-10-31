102a72045778a0863aaef82caa1a1c611c101904
rui-watanabe <wataru610806@gmail.com>
2020-10-31 10:59:07 +0900

    delete count relation folder

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,10 +1,8 @@
 import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
 import covidReducer from "../features/covid/covidSlice";
-import counterReducer from "../features/counter/counterSlice";
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
     covid: covidReducer,
   },
 });
