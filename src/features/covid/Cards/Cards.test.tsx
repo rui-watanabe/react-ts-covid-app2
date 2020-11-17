3a0be199c8e9124e4eed7340b62a4cdf01bbdac4
rui-watanabe <wataru610806@gmail.com>
2020-11-17 22:33:14 +0900

    add should render text infected persons of Cards Component

---
+++ src/features/covid/Cards/Cards.test.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { render, screen, cleanup } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { Provider } from 'react-redux';
+import { configureStore, EnhancedStore } from '@reduxjs/toolkit';
+import covidReducer from '../covidSlice';
+import Cards from './Cards'
+
+afterEach(() => {
+  cleanup();
+});
+
+describe("Redux Integration Test", () => {
+  let store: EnhancedStore<any>;
+  beforeEach(() => {
+    store = configureStore({
+      reducer: {
+        covid: covidReducer
+      }
+    })
+  });
+  it("Should render text that Infected persons", () => {
+    render(<Provider store={store}>
+      <Cards />
+    </Provider>);
+    expect(screen.getByText("Infected persons")).toBeTruthy();
+  })
+})
